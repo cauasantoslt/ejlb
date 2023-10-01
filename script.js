0db2dfc7408d265fbad720c808af379b5158cc8f
Alisonjs0 <josealison163@gmail.com>
2023-10-01 17:00:03 -0300

    menu pronto e algumas animações

---
+++ script.js
@@ -0,0 +1,14 @@
+function clickMenu() {
+  if (itens.style.display == 'block') {
+    itens.style.display = 'none'
+  } else {
+    itens.style.display = 'block'
+  }
+}
+function closeMenu() {
+  if (itens.style.display == 'block') {
+    itens.style.display = 'none'
+  } else {
+    itens.style.display = 'block'
+  }
+}
